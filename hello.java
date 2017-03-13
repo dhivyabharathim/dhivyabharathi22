a6f674c7a179e5cb2e1dcb7c16d8143b014e9eb7
dhivyabharathim <dhivyamariyappan19@gmail.com>
2017-03-13 10:39:59 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,15 @@
+
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char n;
+for(n=1;n<=5;n++)
+{
+	System.out.println("Hello");
+}
+
+	}
+
+}
