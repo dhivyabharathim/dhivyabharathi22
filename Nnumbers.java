5a1ba9d106ea8f569cc7f9289684e2c8e6da7169
dhivyabharathim <dhivyamariyappan19@gmail.com>
2017-03-13 10:57:23 +0530

    Add files via upload

---
+++ Nnumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Nnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,i;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(i=1;i<=n;i++)
+{
+	System.out.println(i);
+}
+
+	}
+
+}
