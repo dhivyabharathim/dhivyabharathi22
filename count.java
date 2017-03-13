e5ad9eb206157b6fc46a146bcab4cb3bd789f5ad
dhivyabharathim <dhivyamariyappan19@gmail.com>
2017-03-13 11:05:07 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+int n1=String.valueOf(n).length();
+System.out.println(n1);
+	}
+
+}
