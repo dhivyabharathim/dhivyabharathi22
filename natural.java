73be45ecd5734b856c73ebd08017bb41c0435ba8
dhivyabharathim <dhivyamariyappan19@gmail.com>
2017-03-13 10:49:33 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class natural {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+if(n>=0){
+	System.out.println("natural number");
+}
+else
+{
+	System.out.println("not natural number");
+}
+	}
+
+}
